35e5c4726eba611f20af66f872d805f9ce6c918b
jourdanmauricio <jourdanmauricio@gmail.com>
2023-10-13 02:22:49 -0300

    gh-pages

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,7 +16,7 @@ const config = {
   baseUrl: '/',
   organizationName: 'jourdanmauricio',
   projectName: 'lumau-docs',
-  deploymentBranch: 'gh-pages',
+  // deploymentBranch: 'gh-pages',
   trailingSlash: false,
 
   onBrokenLinks: 'throw',
